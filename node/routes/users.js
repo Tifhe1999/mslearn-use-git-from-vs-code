8a30b261fa8b1582eec6e104cea831bc2d956566
Nehemiah Akinbowale <nehemiah.akinbowale@outlook.com>
2025-01-13 16:40:32 -0500

    updated response object

--- node/routes/users.js
+++ node/routes/users.js
@@ -7,3 +7,10 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+const express = require('express')
+const router = express.router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello Git!' })
+});
+module.example - router;
